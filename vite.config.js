8036f9bf63ac8361a24aa3d41a85619e6d7708e0
akarsu35 <cumhurakarsu@gmail.com>
2024-11-12 15:18:28 +0300

    uuid

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import polyfillNode from 'rollup-plugin-polyfill-node'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      plugins: [polyfillNode()],
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-        }),
-      ],
+      external: ['uuid'],
     },
   },
 })
