2fd4f1d230cce5d13425b0a8b964ddde91134fb6
akarsu35 <cumhurakarsu@gmail.com>
2024-11-12 15:11:34 +0300

    uuid

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['uuid'],
+      external: [], // 'uuid' buradan çıkarıldı
     },
+  },
+  optimizeDeps: {
+    include: ['uuid'], // uuid'i optimizeDeps altına ekledik
   },
 })
