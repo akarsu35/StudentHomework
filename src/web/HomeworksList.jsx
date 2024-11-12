bb760c0bb55f5975862be64486bd2702193ff5ab
akarsu35 <cumhurakarsu@gmail.com>
2024-11-12 15:32:20 +0300

    uuid

--- src/web/HomeworksList.jsx
+++ src/web/HomeworksList.jsx
@@ -3,7 +3,7 @@ import { supabase } from '../services/supabaseClient'
 import { assignHomeworkToAllStudents } from '../services/homeworkServices'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
-import { v4 as uuidv4 } from 'uuidv4'
+import { v4 as uuidv4 } from 'uuid'
 
 const HomeworkList = () => {
   const [homeworkList, setHomeworkList] = useState([])
